f77d62bad50438ab927ce69264565c8325bdd787
khocherlin <felixharris11@gmail.com>
2025-07-08 20:38:57 +0700

    adding button and webhook

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  async function handleFileChange(event) {
+    console.log("File selected:", event.target.files[0]);
+    const file = event.target.files[0];
+    const formData = new FormData();
+    formData.append("data", file); // 'data' should match n8n's Binary Property Name
+    formData.append("fileName", event.target.files[0].name); // 'data' should match n8n's Binary Property Name
+
+    try {
+      const response = await axios.post(
+        "https://felix1503.app.n8n.cloud/webhook-test/my-webhook",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      console.log(response);
+    } catch (error) {
+      console.error("Error uploading file:", error);
+    }
+  }
 
   return (
     <>
@@ -21,6 +45,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <input type="file" onChange={handleFileChange} />
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
@@ -29,7 +54,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
